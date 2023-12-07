4e31034880347a9b9b4b8b8c166e4efbe4b19474
Hannah-Liao <hanna.liao@outlook.com>
2023-12-07 18:54:02 +0000

    feat: set up routes and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+
 import './App.css';
+import Routers from "./routers/Routers";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
+    </>
   );
 }
 
