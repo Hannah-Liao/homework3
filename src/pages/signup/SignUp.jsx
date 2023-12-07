c9ba78f6a095b19485811953a65338706478e657
Hannah-Liao <hanna.liao@outlook.com>
2023-12-07 19:32:20 +0000

    feat: create sign up page

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+import registerImg from '../../assets/sign_up.svg';
+
 const SignUp = () => {
-	return <div>SignUp</div>;
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const navigate = useNavigate();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		navigate('/login');
+	};
+
+	return (
+		<section className='auth'>
+			<img src={registerImg} alt='' />
+
+			<div className='auth-container'>
+				<form onSubmit={handleSubmit}>
+					<h2>Register</h2>
+					<div className='form-group'>
+						<input
+							type='text'
+							id='username'
+							placeholder='username'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='email'
+							id='email'
+							placeholder='Email'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+
+					<div className='form-group'>
+						<input
+							type='password'
+							id='password'
+							placeholder='Password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<button className='auth_btn' type='submit'>
+						Register
+					</button>
+				</form>
+
+				<p>
+					Already have an account? <Link to='/login'>Login</Link>
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default SignUp;
