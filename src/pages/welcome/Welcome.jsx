78f7874abdecd2cbbb7a17f20cc59ef3c003a2a0
Hannah-Liao <hanna.liao@outlook.com>
2023-12-07 21:19:34 +0000

    feat: create welcome page

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AuthContext } from '../../context/AuthContext.js';
 
 const Welcome = () => {
-	return <div>Welcome</div>;
+	const { dispatch } = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	const logout = () => {
+		dispatch({ type: 'LOGOUT' });
+		navigate('/');
+	};
+
+	return (
+		<section className='welcome'>
+			<h1>Welcome! You log in successfully!</h1>
+
+			<button className='logout_btn' onClick={logout}>
+				SignOut
+			</button>
+		</section>
+	);
 };
 
 export default Welcome;
